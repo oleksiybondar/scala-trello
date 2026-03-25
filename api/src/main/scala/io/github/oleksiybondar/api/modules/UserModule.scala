9071cf50be5468f63b31d645256d23dc140a2190
oleksiybondar <bondaroleksiyandriyovich@gmail.com>
2026-03-25 14:38:20 +0100

    cleanup unused

--- api/src/main/scala/io/github/oleksiybondar/api/modules/UserModule.scala
+++ api/src/main/scala/io/github/oleksiybondar/api/modules/UserModule.scala
@@ -2,9 +2,7 @@ package io.github.oleksiybondar.api.modules
 
 import cats.effect.Async
 import io.github.oleksiybondar.api.domain.user.{UserService, UserServiceLive}
-import io.github.oleksiybondar.api.http.routes.UserRoutes
 import io.github.oleksiybondar.api.infrastructure.db.user.{SlickUserRepo, UserRepo}
-import org.http4s.HttpRoutes
 import slick.jdbc.PostgresProfile.api.Database
 
 import scala.concurrent.ExecutionContext
@@ -12,7 +10,6 @@ import scala.concurrent.ExecutionContext
 final case class UserModule[F[_]](
                                    userRepo: UserRepo[F],
                                    userService: UserService[F],
-                                   routes: HttpRoutes[F]
                                  )
 
 object UserModule {
@@ -25,14 +22,11 @@ object UserModule {
 
     val userService: UserService[F] =
       new UserServiceLive[F](userRepo)
-
-    val routes: HttpRoutes[F] =
-      UserRoutes.routes[F](userService)
+    
 
     UserModule(
       userRepo = userRepo,
       userService = userService,
-      routes = routes
     )
   }
 }
